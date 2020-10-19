12f48dbdf5a9a06232b206d6976161b25d3dec59
Jai Shiv <nsoni19012001@gmail.com>
2020-10-19 12:33:12 +0530

    homepage created.

--- src/requests.js
+++ src/requests.js
@@ -2,8 +2,8 @@ const APIKEY = "bb48c9dcfc0869f7b78715b2438282f1"
 
 const requests = {
     fetchTrending: `/trending/all/week?api_key=${APIKEY}&language=en-US`,
-    fetchNetflixOrigionals: `/discover/tv?api_key=${APIKEY}&with=networks=213`,
-    fetchTopRated: `/movie/top_related?api_key=${APIKEY}&language=en-US`,
+    fetchNetflixOriginals: `/discover/tv?api_key=${APIKEY}&with=networks=213`,
+    fetchTopRated: `/movie/top_rated?api_key=${APIKEY}&language=en-US`,
     fetchActionMovies: `/discover/movie?api_key=${APIKEY}&with_genres=28`,
     fetchComedyMovies: `/discover/movie?api_key=${APIKEY}&with_genres=35`,
     fetchHorrorMovies: `/discover/movie?api_key=${APIKEY}&with_genres=27`,
